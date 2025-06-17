feb8613494d15f9c7ea84af26033f2f941413422
MiguelA536 <2410307@alumnos.univo.edu.mx>
2025-06-16 20:46:27 -0600

    Aqui se subirá el progreso del programa cuando este terminado

---
+++ src/main/java/com/mycompany/tareaclase5/TareaClase5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareaclase5;
+
+/**
+ *
+ * @author migue
+ */
+public class TareaClase5 {
+
+    public static void main(String[] args) {
+        System.out.println("El siguiente programa muestra un ejemplo de grafo y uno de arbol binario");
+        Menu m1 = new Menu();
+    }
+}
